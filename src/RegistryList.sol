141f698cf790f1bcbc1e93fb449ad03938a8badd
MathisGD <mathis@morpho.xyz>
2025-09-02 16:34:08 +0200

    style: renamings

---
+++ src/RegistryList.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+// Copyright (c) 2025 Morpho Association
+pragma solidity 0.8.28;
+
+import "./interfaces/IRegistryList.sol";
+
+contract RegistryList is IRegistryList {
+    address public owner;
+
+    /// @dev Owner controlled append-only list of subRegistries.
+    address[] public subRegistries;
+
+    function subRegistriesLength() external view returns (uint256) {
+        return subRegistries.length;
+    }
+
+    event Constructor(address indexed owner);
+    event SetOwner(address indexed newOwner);
+    event AddSubRegistry(address indexed subRegistry);
+
+    constructor() {
+        owner = msg.sender;
+        emit Constructor(msg.sender);
+    }
+
+    function setOwner(address newOwner) external {
+        require(msg.sender == owner, "Not owner");
+        owner = newOwner;
+        emit SetOwner(newOwner);
+    }
+
+    function addSubRegistry(address subRegistry) external {
+        require(msg.sender == owner, "Not owner");
+        subRegistries.push(subRegistry);
+        emit AddSubRegistry(subRegistry);
+    }
+
+    function isInRegistry(address adapter) public view returns (bool) {
+        for (uint256 i = 0; i < subRegistries.length; i++) {
+            if (IRegistryList(subRegistries[i]).isInRegistry(adapter)) return true;
+        }
+        return false;
+    }
+}
