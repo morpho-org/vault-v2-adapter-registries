aa69ed11eb13c47c50dedcad1aef11a6fca26208
MathisGD <mathis@morpho.xyz>
2025-09-04 16:36:03 +0200

    chore: remove vaultV2 registry

--- src/interfaces/IMorphoVaultV2Registry.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-// Copyright (c) 2025 Morpho Association
-pragma solidity 0.8.28;
-
-import "../../lib/vault-v2/src/interfaces/IAdapterRegistry.sol";
-
-interface IMorphoVaultV2Registry is IAdapterRegistry {
-    function expectedRegistry() external view returns (address);
-}
